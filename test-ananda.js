9a3eec134f7fa750d4bb390a4a09f54a6c1a936a
dev-Anamul <ah9737677@gmail.com>
2022-08-21 23:36:28 +0600

    add log fill

--- test-ananda.js
+++ test-ananda.js
@@ -1,2 +1,3 @@
-console.log('test collaboration');
-console.log('test collaboration 2');
+console.log("test collaboration");
+console.log("test collaboration 2");
+console.log("test collaboration 3");
